3ed8ee571ab0801d202f6c8ca9918415128c0617
kauazn23 <kauasouzq6@gmail.com>
2026-09-23 22:37:08 -0400

    roda GET "/tarefas/:id" criada

--- index.js
+++ index.js
@@ -6,21 +6,32 @@ const PORT = 3000
 
 const tarefas = [
     {
-        id: 1, titulo: 'Estudar PTAS', concluida: true
+        id: 1,
+        titulo: 'Estudar Express', concluida: true
     },
     {
         id: 2, titulo: 'Fazer exercícios', concluida: false
     },
     {
-        id: 3, titulo: 'Estudar Matemática', concluida: false
+        id: 3, titulo: 'Estudar JavaScript', concluida: false
     }]
-
 app.get('/', (req, res) => {
     res.send('API de Tarefas no ar')
 })
 
 app.get('/tarefas', (req, res) => {
     res.json(tarefas)
+})
+
+app.get('/tarefas/:id', (req, res) => {
+    const id = Number(req.params.id)
+
+    const tarefa = tarefas.find(tarefa => tarefa.id === id) 
+    if (!tarefa) {
+        return res.status(404).json({
+            erro: 'Tarefa não encontrada'})
+    }
+    res.json(tarefa)
 })
 
 app.listen(PORT, () => {
