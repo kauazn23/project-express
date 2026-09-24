d4a3c80bfdb715055373aa202e0bfe8582e8b2c3
kauazn23 <kauasouzq6@gmail.com>
2026-09-23 22:23:44 -0400

    rota GET tarefas criada

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const app = express()
 
 const PORT = 3000
 
+const tarefas = [
+    {
+        id: 1, titulo: 'Estudar PTAS', concluida: true
+    },
+    {
+        id: 2, titulo: 'Fazer exercícios', concluida: false
+    },
+    {
+        id: 3, titulo: 'Estudar Matemática', concluida: false
+    }]
+
 app.get('/', (req, res) => {
     res.send('API de Tarefas no ar')
+})
+
+app.get('/tarefas', (req, res) => {
+    res.json(tarefas)
 })
 
 app.listen(PORT, () => {
